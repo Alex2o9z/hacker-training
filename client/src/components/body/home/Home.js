7d83978f2dd3964c40130f12fa803c08b12e8357
Ho Thang Trinh <hothangtrinh200902@gmail.com>
2022-11-24 17:51:23 +0700

    first-commit
    first-commit

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <p>
+                Wish you have a good day. Thank you very much!
+            </p>
+            {/* <a href="" target="_blank" 
+            rel="noopener noreferrer">Link</a> */}
+        </div>
+    )
+}
+
+export default Home
---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email!"],
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password!"]
+    },
+    role: {
+        type: Number,
+        default: 0 // 0 = user, 1 = admin
+    },
+    avatar: {
+        type: String,
+        default: "https://st3.depositphotos.com/1007566/12989/v/950/depositphotos_129895474-stock-illustration-hacker-character-avatar-icon.jpg"
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Users", userSchema)
